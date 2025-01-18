be0c09d1849825173f68524ceef56952eb1126ac
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-01-18 11:16:09 +0530

    Initial Commit

--- CppBasics/Conditionals/ifelse.cpp
+++ CppBasics/Conditionals/ifelse.cpp
@@ -5,6 +5,21 @@ using namespace std;
 int main(){
     int num; //input
     cin>>num;
-    if(num%2==0) cout<<"EVEN";
+
+    // //type1
+    // if(num%2==0) cout<<"EVEN";    
+    // else cout<<"ODD";
+
+    // //type2
+    // if((num/2)*2==num){
+    //     cout<<"EVEN";
+    // }
+    // else cout<<"ODD";
+    
+    //type3
+    int half=num/2;
+    if(half+half==num) cout<<"EVEN";
     else cout<<"ODD";
+
+
 }
