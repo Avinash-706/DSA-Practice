05353cc2355b35efb6469def899c0f4b796944ad
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-01-19 19:54:07 +0530

    Latest Commit

---
+++ Array/sortedCheck.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+bool issorted(int arr[], int n){
+    for(int i=0; i<n-1; i++){ 
+        if(arr[i] > arr[i+1]){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    int n=5;
+    int arr[5]={10,20,37,40,155};
+    bool check=issorted(arr, n);
+    cout << check;
+}
+//
