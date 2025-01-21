97518d3b6d5ef210088934f827dc4b73debcc527
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-01-21 11:28:20 +0530

    Latest Commit

--- Array/removeduplicates.cpp
+++ Array/removeduplicates.cpp
@@ -4,24 +4,27 @@
 using namespace std;
 
 int main() {
-    int n;
+    int n=6;
     cout << "Enter the size of the array: " << endl;
-    cin >> n;
-    int arr[n];
+    // cin >> n;
+    int arr[6]={10,20,50,10,20,70};
 
-    for(int i=0 ; i < n ; i++){
-        cin >> arr[i];
-    }
+    // for(int i=0 ; i < n ; i++){
+    //     cin >> arr[i];
+    // }
     sort(arr, arr+n); 
-
+    
+    //sorted array
     for(int i=0; i < n; i++){
         cout << arr[i] << "  ";
     }
 
+    //Duplicate Check
+    cout<<"Duplicate Check";
     for(int i=0; i<n; i++){
         for(int j=i+1; j<n; j++){
             if(arr[i]==arr[j]){
-                cout<<arr[i];
+                cout<<arr[i]<<"   ";
             }
         }
     }
