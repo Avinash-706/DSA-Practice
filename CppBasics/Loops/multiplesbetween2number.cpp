3cf87a5911a4325092d6a71c81b7a7c40b5dc4af
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-01-21 09:30:01 +0530

    Latest Commit

--- CppBasics/Loops/multiplesbetween2number.cpp
+++ CppBasics/Loops/multiplesbetween2number.cpp
@@ -4,11 +4,13 @@
 using namespace std;
 
 int main() {
-    int a, b, n;
+    int a, b, n, count=0;
     cout << "Enter two numbers followed by N: ";
     cin >> a >> b >> n;
-    for(int i=a; i <= b; i++){
-        if(i % n == 0) cout <<i << endl;
+    for(int i=a; i <= b; i++){ //INCLUSIVE of 2 numbers also
+        if(i % n == 0) {cout << i << endl; count++;}
     }
+    cout << "Total Number of Multiples between "<< a<< " and "<<b<<" : " <<count;
     return 0;
+
 }
