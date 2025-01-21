84492708df85ab0b32ca82f9bb6b56d310e5f0a1
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-01-22 00:26:55 +0530

    Latest Commit

--- Array/move0toEnd.cpp
+++ Array/move0toEnd.cpp
@@ -9,6 +9,7 @@ int moveZero(int arr[], int n){
             for(int j=i+1; j<n ; j++){
                 if(arr[j] != 0){
                     swap(arr[i], arr[j]);
+                    j++;
                 }
             }
         }
@@ -19,7 +20,7 @@ int main() {
     int n=6;
     int arr[6]={10, 0, 20, 0, 0, 50};
     // int n;
-    // cin>>n;
+    cin>>n;
     // int arr[n];
     // cout<<"Enter the size of the array: ";
     // for(int i=0; i<n; i++){
