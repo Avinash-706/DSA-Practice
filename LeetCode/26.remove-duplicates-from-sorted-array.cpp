df6eb9d0f5188d4a517eda879ea85c51da639425
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-01-23 11:49:41 +0530

    Latest Commit

--- LeetCode/26.remove-duplicates-from-sorted-array.cpp
+++ LeetCode/26.remove-duplicates-from-sorted-array.cpp
@@ -12,10 +12,16 @@ using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        for(int i=0; i<nums.size(); i++){
-            
+        int k=1;
+        for(int i=1; i<nums.size(); i++){
+            if(nums[k-1]!=nums[i]){
+                nums[k]=nums[i];
+                k++;
+            }
         }
+        return k;
     }
+    
 };
 // @lc code=end
 
