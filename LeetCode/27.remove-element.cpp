6bd31fbef9c71ac7e9f989e93949a965381c0c39
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-01-23 11:51:35 +0530

    Latest Commit

--- LeetCode/27.remove-element.cpp
+++ LeetCode/27.remove-element.cpp
@@ -5,10 +5,19 @@
  */
 
 // @lc code=start
+#include <iostream>
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        
+        int k=1;
+        for(int i=0; i<nums.size(); i++){
+
+        }
     }
 };
 // @lc code=end
