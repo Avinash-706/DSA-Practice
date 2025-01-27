50fdc2666680a9c3f6b398b7ccfdfcbbbf7e277e
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-01-27 19:17:01 +0530

    Latest Commit

---
+++ Array/move0toEndOPTIMIZED.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+
+//OPTIMIZED APPROACH
+int moveZero(int arr[], int n){
+    int count=0;
+    for(int i=0; i<n; i++){
+        if(arr[i]!=0){
+            swap(arr[i], arr[count]);
+            count++;
+        }
+    }
+}
+
+int main() {
+    int n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    moveZero(arr, n);
+
+    for(int i=0; i<n; i++){
+        cout << arr[i] << "  ";
+    }
+    return 0;
+}
+
