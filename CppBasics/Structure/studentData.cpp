553e481d1335b75a8f9b0cdbda2bf7cf7b6ef9c3
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-01-28 11:48:51 +0530

    Latest Commit

--- CppBasics/Structure/studentData.cpp
+++ CppBasics/Structure/studentData.cpp
@@ -2,16 +2,55 @@
 #include <bits/stdc++.h>
 #include <iomanip>
 #include <algorithm>
+#include <string>
+#include <string.h>
+#include <limits>
+#include <limits.h>
 using namespace std;
 
-void calculate(float marks[], float 
+struct studentData
+{
+    string name;
+    float marks[5], total=0, perc=0;
+    int roll;
+};
 
-void display(string &name, float marks[], float perc, float ){
-    cout<<name<<endl<< roll<<endl<<total<<endl<<perc;
+void input(studentData &s){
+    cout<<"Enter name: ";
+    getline(cin, s.name);
+    cout<<"Enter roll: ";
+    cin>>s.roll;
+    cout<<"Enter Marks of all 5: ";
+    cin>>s.marks[0]>>s.marks[1]>>s.marks[2]>>s.marks[3]>>s.marks[4];
 }
 
-int main() {
-    int n;
-
-    return 0;
+void calculate(studentData &s){
+    s.total=s.marks[0]+s.marks[1]+s.marks[2]+s.marks[3]+s.marks[4];
+    s.perc=s.total/5;
 }
+
+void display(studentData &s){
+    cout<<"Total Marks: "<<s.total<<endl;
+    cout<<"Percentage: "<<s.perc<<endl;
+}
+
+int main(){
+    studentData s;
+    input(s);
+    calculate(s);
+    display(s);
+}
+
+
+
+// void calculate(float marks[], float 
+
+// void display(string &name, float marks[], float perc, float ){
+//     cout<<name<<endl<< roll<<endl<<total<<endl<<perc;
+// }
+
+// int main() {
+//     int n;
+
+//     return 0;
+// }
