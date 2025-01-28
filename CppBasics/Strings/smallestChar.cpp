651e8716f3e396ece3972c53a367614aba8f85ab
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-01-28 10:06:59 +0530

    Latest Commit

--- CppBasics/Strings/smallestChar.cpp
+++ CppBasics/Strings/smallestChar.cpp
@@ -4,19 +4,40 @@
 #include <algorithm>
 #include <string.h>
 #include <strings.h>
-
 using namespace std;
 
-char smallest_Char(string str){
+int countChar(string str, char n){
+    int count=0;
+    for(int i=0; i<str.length(); i++) if(str[i] == n)  count++;
+    return count;
+}
+
+int smallest_Char(string str){
     char smallest=str[0];
+    int index=0;
     for(int i=1; i < str.length(); i++){
-        if(smallest>str[i]){
+        if(smallest>str[i] && str[i]!=' '){ //removing spaces from string
             smallest=str[i];
+            index=i;
         }
     }
-    return smallest;
-
+    return index;
 }
+
+
+int largest_Char(string str){
+    char largest=str[0];
+    int index=0;
+    for(int i=1; i < str.length(); i++){
+        if(largest<str[i]){
+            largest=str[i];
+            index=i;
+        }
+    }
+    return index;
+}
+
+
 int length(string &str){
     int len=0;
     while(str[len]!='\0'){ // or while(str[len]){}
@@ -26,18 +47,22 @@ int length(string &str){
 }
 
 
-
 void  input(string &str){
-    cout<< "\n Enter String: ";
+    cout<< "\nEnter String: ";
     getline(cin, str);
     cout << "Length of the string: "<< length(str) << endl;
-
 }
 
 int main() {
     string str;
-    cout<<str.length()<<endl;
     input(str);
-    cout << "Smallest Character: " << smallest_Char(str);
+    cout << "Smallest Character: " << str[smallest_Char(str)] << endl;
+    cout << "ASCII value of Smallest Character: " << int(str[smallest_Char(str)]) <<endl;
+    cout << "Largest Character: " << str[largest_Char(str)] << endl;
+    cout << "ASCII value of Largest Character: " << int(str[largest_Char(str)]) << endl;
+    char countOccurences;
+    cout << "Enter the character to find the occurences: ";
+    cin >> countOccurences;
+    cout << "Count of " << countOccurences <<" Character: " << countChar(str, countOccurences) << endl;
     return 0;
 }
