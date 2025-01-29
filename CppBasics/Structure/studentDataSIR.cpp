b22657700b24bd5e83e9c131ab4a2f74655741f4
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-01-29 09:45:58 +0530

    Latest Commit

--- CppBasics/Structure/studentDataSIR.cpp
+++ CppBasics/Structure/studentDataSIR.cpp
@@ -2,74 +2,93 @@
 #include <limits>
 using namespace std;
 
-
 struct student_details
 {
     string name;
-    float marks[5], total=0, perc=0;
+    float marks[5], total=0, perc=0, fine=0;
     int roll;
+    char grade;
 };
 
-
 void input(student_details &s)
-{   cin.ignore(numeric_limits<streamsize>::max(),'\n');
+{   
+    cin.ignore(numeric_limits<streamsize>::max(),'\n');
     cout<<"\nEnter Name - ";
-    getline(cin,s.name);
+    getline(cin, s.name);
     cout<<"\nEnter Roll - ";
-    cin>>s.roll;
-    cout<<"\nEnter Marks of all 5 - ";
-    cin>>s.marks[0]>>s.marks[1]>>s.marks[2]>>s.marks[3]>>s.marks[4];
+    cin >> s.roll;
+    cout<<"\nEnter Marks of all 5 subjects - ";
+    for(int i = 0; i < 5; i++) cin >> s.marks[i];
 }
 
+void display(student_details &s)
+{   
+    cout << "\n----- Student Details -----\n";
+    cout << "Name: " << s.name << endl;
+    cout << "Roll: " << s.roll << endl;
+    cout << "Total Marks: " << s.total << endl;
+    cout << "Percentage: " << s.perc << "%" << endl;
+    cout << "Grade: " << s.grade << endl;
+    cout << "Fine: " << s.fine << " Rs\n";
+}
+
+int fineCalculator(student_details &s)
+{
+    int totalFine = 0;
+    if (s.grade == 'F') totalFine += 5000;
+    
+    for (int i = 0; i < 5; i++)  
+        if (s.marks[i] < 50)  
+            totalFine += 500;
+
+    return totalFine;
+}
 
 void calculate(student_details &s)
 {
-    s.total=s.marks[0]+s.marks[1]+s.marks[2]+s.marks[3]+s.marks[4];
-    s.perc=s.total/5;
+    s.total = 0;
+    for(int i = 0; i < 5; i++) 
+        s.total += s.marks[i];
 
+    s.perc = s.total / 5;
+    s.grade = (s.perc > 50) ? 'P' : 'F';
+    
+    s.fine = fineCalculator(s);
 }
 
-
-void display(student_details &s)
-{   cout<<endl<<"Students Details \n";
-    cout<<s.name<<endl<<s.roll<<endl<<s.total<<endl<<s.perc;
-}
-
-int topper(student_details s[], int n){
-    float max= s[0].perc;
-    int index=0;
-    for(int i=0; i<n; i++){
-        if(s[i].perc > max ){
-            max=s[i].perc;
-            index=i;
+int topper(student_details s[], int n)
+{
+    float max = s[0].perc;
+    int index = 0;
+    for(int i = 1; i < n; i++)
+    {
+        if(s[i].perc > max)
+        {
+            max = s[i].perc;
+            index = i;
         }
     }
     return index;
 }
 
-
 int main()
-{   student_details s[5];
+{   
+    student_details s[5];
     int n;
-    cout<<"Enter number of students max 5 ";
-    cin>>n;
+    cout << "Enter number of students (max 5): ";
+    cin >> n;
     
-    for (int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
         input(s[i]);
-        calculate (s[i]);
-        //display(s[i]);
+        calculate(s[i]);
     }
-    for (int i=0;i<n;i++)
-    {
-        //input(s[i]);
-        //calculate (s[i]);
+    
+    for (int i = 0; i < n; i++)
         display(s[i]);
-    }
-    cout << "---- DETAILS OF THE TOPPER STUDENT ----";
+
+    cout << "\n---- DETAILS OF THE TOPPER STUDENT ----\n";
     display(s[topper(s, n)]);
-
-
 
     return 0;
 }
