92d7d9451275892d6c1e7f1b28b8a6d4e72a3d88
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-01-30 23:01:45 +0530

    Latest Commit

--- Array/stockBuySellOPTIMIZED.cpp
+++ Array/stockBuySellOPTIMIZED.cpp
@@ -11,17 +11,24 @@ using namespace std;
 int stockBuySell(int n, int arr[])
 {
     int profit = 0;
-    for (int i = 0; i < n; i++){
+    int left = 0;
+    for (int i = 1; i < n; i++){
         if(arr[i] > arr[i-1]){
             profit += arr[i]-arr[i-1];
+            if(i == n - 1)  cout << left + 1 << " -> " << i + 1  << endl;
+        }
+        else {
+            if(left != i-1)   cout << left + 1 << " -> " << i  << endl;
+            
+            left=i;
         }
     }
     return profit;
 }
 
 int main(){
-    int n = 7;
-    int arr[7] = {100, 180, 260, 310, 40, 535, 695};
+    int n = 6;
+    int arr[6] = {7, 1, 5, 3, 6, 4};
 
     // int n;
     // cout << "Enter the size of the array: ";
