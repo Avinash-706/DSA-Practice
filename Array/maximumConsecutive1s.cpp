61ff9364a6fc5899c28ac29471c69c599152b1d5
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-01-31 22:49:42 +0530

    Latest Commit

--- Array/maximumConsecutive1s.cpp
+++ Array/maximumConsecutive1s.cpp
@@ -11,21 +11,14 @@ using namespace std;
 int maximumConsecutive1s(int n, int arr[]){
     int max_count=0;
     int count = 0;
-    //1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1
     for(int i = 0 ; i < n ; i++){
-        if(arr[i] == 1 && arr[i+1] == 1){
-            // if (max_count < count)  max_count=count;
-            // count=0;
-            // cout<<arr[i]<<"   ";
-            count++;
-        }
+        if(arr[i] == 0)    count = 0;
         else{
-            if (max_count < count)  max_count=count;
-            count=0;
+            count++;
+            max_count=max(max_count, count);
         }
     }
-    if (max_count < count)  max_count=count;
-    return max_count+1;
+    return max_count;
 
 }
 
