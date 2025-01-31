0af917bcf86180a254089b07ec51d75ea59ce12d
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-01-31 23:06:11 +0530

    Latest Commit

---
+++ Array/maximumSubarraySumOPTIMIZED.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <string.h>
+#include <limits>
+using namespace std;
+
+//2, 3, -8, 7, -1, 2, 3
+//11
+
+int maximumSubarraySum(int n, int arr[]){
+    int maxSum=0;
+    for(int i = 0 ; i < n ; i++){
+        int sum=0;
+        for(int j = i ; j < n ; j++){
+            sum = sum + arr[j];
+            maxSum = max(maxSum, sum);
+        }
+    }
+    return maxSum;
+}
+
+
+int main() {
+    int n=7;
+    int arr[7]={2, 3, -8, 7, -1, 2, 3};
+
+    // int n;
+    // cout << "Enter the size of the array: ";
+    // cin >> n;
+    // int arr[n];
+    // for(int i=0; i < n; i++){
+    //     int input;
+    //     cin >> input;
+    //     arr[i] = input;
+    // }
+
+    cout<<maximumSubarraySum(n, arr);
+    return 0;
+}
