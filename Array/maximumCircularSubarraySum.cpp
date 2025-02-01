ced843a828060a9a38d111d3099aab4bb150e703
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-02-01 23:45:59 +0530

    Latest Commit

--- Array/maximumCircularSubarraySum.cpp
+++ Array/maximumCircularSubarraySum.cpp
@@ -7,15 +7,29 @@
 #include <limits>
 using namespace std;
 
+// 5, -2, 3, 4
+// 12
 
-void compute(int n, int arr[]){
-   
+//UNOPTIMIZED
+int maximumCircularSubarraySum(int n, int arr[]){
+    int result=arr[0];
+    for(int i = 0 ; i < n ; i++){
+        int curr_max = arr[i];
+        int curr_sum = arr[i];
+        for(int j = 1 ; j < n ; j++){
+            int index = (i + j) % n;
+            curr_sum += arr[index];
+            curr_max = max(curr_max, curr_sum);
+        }
+        result = max(curr_max, result);
+    }
+    return result;
 }
 
-
 int main() {
-    int n=6;
-    int arr[6]={7, 9, 5, 6, 3, 2};
+    int n=4;
+    int arr[4]={5, -2, 3, 4};
+    
     
     // int n;
     // cout << "Enter the size of the array: ";
@@ -27,6 +41,9 @@ int main() {
     //     arr[i] = input;
     // }
 
-    compute(n, arr);
+    cout<<maximumCircularSubarraySum(n, arr);
     return 0;
 }
+
+//TIME COMPLEXITY - O(n²), where n is the size of the array.
+//SPACE COMPLEXITY - O(1)
