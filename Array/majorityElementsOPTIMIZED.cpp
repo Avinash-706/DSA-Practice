9e4e73f530224d050a9c47cfe067ae60a1cba290
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-02-02 19:48:20 +0530

    Latest Commit

--- Array/majorityElementsOPTIMIZED.cpp
+++ Array/majorityElementsOPTIMIZED.cpp
@@ -7,30 +7,48 @@
 #include <limits>
 using namespace std;
 
-//UNOPTIMIZED APPROACH
+// IN - 8, 7, 6, 8, 6, 6, 6, 6
+// OUT - 6
+
+//OPTIMIZED APPROACH
 int majorityElements(int n, int arr[]){
-    for(int i = 0 ; i < n ; i++){
-        int count=1;
-        for(int j = i + 1 ; j < n ; j++)    if(arr[i] == arr[j])    count++;
-        if(count > n/2) return i;
+    int res=0;
+    int count=1;
+
+    for(int i = 1 ; i < n ; i++){
+
+        if (arr[res]==arr[i])   count++;
+        else    count--;
+        if (count == 0){
+            count=1;
+            res=i;
+        }
+    }
+
+    count=0;
+    for(int i=0 ; i<n ; i++){
+        if(arr[res] == arr[i]){
+            count++;
+        }
+        if(count > n/2) return res;
     }
     return -1;
 }
 
 
 int main() {
-    int n=8;
-    int arr[8]={8, 7, 6, 8, 6, 6, 6, 6};
+    // int n=8;
+    // int arr[8]={8, 7, 6, 8, 6, 6, 6, 6};
 
-    // int n;
-    // cout << "Enter the size of the array: ";
-    // cin >> n;
-    // int arr[n];
-    // for(int i=0; i < n; i++){
-    //     int input;
-    //     cin >> input;
-    //     arr[i] = input;
-    // }
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    for(int i=0; i < n; i++){
+        int input;
+        cin >> input;
+        arr[i] = input;
+    }
 
     cout<<"Majority Element : "<<arr[majorityElements(n, arr)];
     return 0;
