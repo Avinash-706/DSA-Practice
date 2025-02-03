744d50d3147901f1e8521e615f666549b961b065
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-02-03 19:38:24 +0530

    Latest Commit

---
+++ Array/maxSumofKconsecutives.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <string.h>
+#include <limits>
+using namespace std;
+
+// IN -  1, 8, 30, -5, 20, 7
+// OUT - 45
+
+int maxSumofKconsecutivesElements(int n, int k, int arr[]){
+    int maxSum=0;
+    for(int i = 0 ; i <= n - k  ; i++){
+        int checkSum = 0;
+        for(int j = i ; j < k + i ; j++ ){
+            checkSum += arr[j];
+        }
+        maxSum = max( maxSum, checkSum);
+        // cout<< endl;
+    }
+    return maxSum;
+}
+
+
+int main() {
+    // int n=5;
+    // int k=2;
+    // int arr[5]={5, -10, 6, 90, 3};
+
+    int n, k;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    cout << " Enter the K consecutive Elements: ";
+    cin >> k;
+    int arr[n];
+    for(int i=0; i < n; i++){
+        int input;
+        cin >> input;
+        arr[i] = input;
+    }
+
+    cout << maxSumofKconsecutivesElements(n, k, arr);
+    return 0;
+}
---
+++ CppBasics/GenericProgramming/genericProgram.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <string.h>
+#include <limits>
+using namespace std;
+
+
+void compute(int n, int arr[]){
+   
+}
+
+
+int main() {
+    int n=6;
+    int arr[6]={7, 9, 5, 6, 3, 2};
+
+    // int n;
+    // cout << "Enter the size of the array: ";
+    // cin >> n;
+    // int arr[n];
+    // for(int i=0; i < n; i++){
+    //     int input;
+    //     cin >> input;
+    //     arr[i] = input;
+    // }
+
+    compute(n, arr);
+    return 0;
+}
