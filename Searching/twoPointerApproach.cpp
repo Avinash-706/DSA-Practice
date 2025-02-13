a96fdc4c80f51460cc2721b01fe10ace8ae71c42
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-02-13 20:13:38 +0530

    Latest Commit

--- Searching/twoPointerApproach.cpp
+++ Searching/twoPointerApproach.cpp
@@ -8,29 +8,39 @@
 using namespace std;
 
 //INPUT MUST BE IN SORTED ORDER --
-void compute(int n, int arr[]){
-   
+bool compute(int n, int arr[], int number){
+    int low=0, high = n-1;
+    while(low < high){
+        int sum = arr[low] + arr[high];
+        if      ( sum == number)    return true;
+        else if ( sum < number )    low ++;
+        else    high--; 
+    }
+    return false;
 }
 
 
 int main() {
-    int n = 6;
-    int arr[6] = {10, 20, 30, 40, 50, 60};
-    int number = 20;
+    // int n = 8;
+    // int arr[8] = {2, 4, 8, 9, 11, 12, 20, 30};
+    // int number = 23;
 
-    // int n, number;
-    // cout << "Enter the size of the array: ";
-    // cin >> n;
-    // int arr[n];
-    // cout << "Enter the elements in the increasing order ( Note: sorted order): ";
-    // for(int i=0; i < n; i++){
-    //     int input;
-    //     cin >> input;
-    //     arr[i] = input;
-    // }
-    // cout << "\nEnter the Number to search from the Given Number: ";
-    // cin >> number;
+    int n, number;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements in the increasing order ( Note: sorted order): ";
+    for(int i=0; i < n; i++){
+        int input;
+        cin >> input;
+        arr[i] = input;
+    }
+    cout << "\nEnter a Sum of Any Two Number from the Given Numbers: ";
+    cin >> number;
 
-    compute(n, arr);
+    cout << (compute(n, arr, number) ? "True" : "False"); 
     return 0;
 }
+
+//TIME COMPLEXITY  - O(n), where 'n' is the size of the array
+//SPACE COMPLEXITY - O(1)
