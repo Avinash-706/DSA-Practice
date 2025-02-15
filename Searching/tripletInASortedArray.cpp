b0ac6d37da05073042289c50bdc687c5f802041f
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-02-15 22:47:33 +0530

    Latest Commit

---
+++ Searching/tripletInASortedArray.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <string.h>
+#include <limits>
+using namespace std;
+
+
+void compute(int n, int arr[], int tripletSum){
+    int sum=0;
+    for(int)
+
+}
+
+
+int main() {
+    int n = 6;
+    int arr[6] = {10, 20, 30, 40, 50, 60};
+    int tripletSum = 20;
+
+    // int n, tripletSum;
+    // cout << "Enter the size of the array: ";
+    // cin >> n;
+    // int arr[n];
+    // cout << "Enter the elements in the increasing order ( Note: sorted order): ";
+    // for(int i=0; i < n; i++){
+    //     int input;
+    //     cin >> input;
+    //     arr[i] = input;
+    // }
+    // cout << "\nEnter the Sum of any 3 number search from the Given Number: ";
+    // cin >> tripletSum;
+
+    compute(n, arr, tripletSum);
+    return 0;
+}
