ea73e5ad0d7a32726a6aec1eba73f986c3af9429
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-02-17 20:14:13 +0530

    Latest Commit

--- Searching/medianOf2UnequalSortedArrayUNOPTIMIZED.cpp
+++ Searching/medianOf2UnequalSortedArrayUNOPTIMIZED.cpp
@@ -13,9 +13,9 @@ using namespace std;
 
 //UNOPTIMIZED APPROACH
 double medianOf2UnequalSortedArrayUNOPTIMIZED(int n1, int n2, int arr1[], int arr2[]){
-
     int n = n1+n2;
     int final[n];
+
     for(int i=0 ; i < n ; i++){
         if(i < n1)   final[i] = arr1[i];
         else if(i >= n1) final[i] = arr2[i - n1]; 
@@ -26,35 +26,35 @@ double medianOf2UnequalSortedArrayUNOPTIMIZED(int n1, int n2, int arr1[], int ar
     else    return  double(final[n/2]);  
 }
 
-// void input(int n, int arr[]){
-//     cout << "Enter the elements in the increasing order ( Note: sorted order): ";
-//     for(int i=0; i < n; i++){
-//         int input;
-//         cin >> input;
-//         arr[i] = input;
-//     }
-// }
+
+void input(int n, int arr[]){
+    cout << "Enter the elements in the increasing order ( Note: sorted order): ";
+    for(int i=0; i < n; i++){
+        int input;
+        cin >> input;
+        arr[i] = input;
+    }
+}
+
 
 int main() {
-    int n1 = 6, n2 = 5;
-    int arr1[6] = {1, 2, 3, 4, 5, 6};
-    int arr2[5] = {10, 20, 30, 40, 50 };
+    // int n1 = 6, n2 = 5;
+    // int arr1[6] = {1, 2, 3, 4, 5, 6};
+    // int arr2[5] = {10, 20, 30, 40, 50 };
 
-    // int n1, n2;
-    // cout << "Enter the size of the array1: ";
-    // cin >> n1;
-    // int arr1[n1];
-    // input(n1, arr1);
-    // cout << "Enter the size of the array2: ";
-    // cin >> n2;
-    // int arr2[n2];
-    // input(n2, arr2);
+    int n1, n2;
+    cout << "Enter the size of the array1: ";
+    cin >> n1;
+    int arr1[n1];
+    input(n1, arr1);
+    cout << "Enter the size of the array2: ";
+    cin >> n2;
+    int arr2[n2];
+    input(n2, arr2);
 
-
-
-
-    cout << fixed << setprecision(2) << medianOf2UnequalSortedArrayUNOPTIMIZED(n1, n2, arr1, arr2);
+    cout << "MEDIAN: " << fixed << setprecision(2) << medianOf2UnequalSortedArrayUNOPTIMIZED(n1, n2, arr1, arr2);
     return 0;
 }
 
-//TIME
+//TIME COMPLEXITY  - O(n log n), where 'n' = 'n1' + 'n2', where 'n1' -> size of array1, 'n2' -> size of the array2
+//SPACE COMPLEXITY - O(n), where 'n' = 'n1' + 'n2', where 'n1' -> size of array1, 'n2' -> size of the array2
