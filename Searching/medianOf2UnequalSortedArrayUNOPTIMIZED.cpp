47ebdb60948c28ca2e6615e339c79d69d89dbcdf
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-02-17 20:10:43 +0530

    Latest Commit

--- Searching/medianOf2UnequalSortedArrayUNOPTIMIZED.cpp
+++ Searching/medianOf2UnequalSortedArrayUNOPTIMIZED.cpp
@@ -7,19 +7,23 @@
 #include <limits>
 using namespace std;
 
+//IN  - 10, 20, 30, 40, 50
+//IN  - 5 , 15, 25, 35, 45
+//OUT - 27.5
 
+//UNOPTIMIZED APPROACH
 double medianOf2UnequalSortedArrayUNOPTIMIZED(int n1, int n2, int arr1[], int arr2[]){
+
     int n = n1+n2;
     int final[n];
     for(int i=0 ; i < n ; i++){
         if(i < n1)   final[i] = arr1[i];
         else if(i >= n1) final[i] = arr2[i - n1]; 
     }
+
     sort(final, final + n);
-    if(n % 2 == 0)    return    (final[n / 2] + final[(n / 2) + 1])/2;
-    else    return  final[n/2];  
-
-
+    if(n % 2 == 0)    return    ((double(final[n / 2]) + double(final[(n / 2) - 1])) / 2.0);
+    else    return  double(final[n/2]);  
 }
 
 // void input(int n, int arr[]){
@@ -32,9 +36,9 @@ double medianOf2UnequalSortedArrayUNOPTIMIZED(int n1, int n2, int arr1[], int ar
 // }
 
 int main() {
-    int n1 = 6, n2 = 6;
+    int n1 = 6, n2 = 5;
     int arr1[6] = {1, 2, 3, 4, 5, 6};
-    int arr2[6] = {9, 20, 30, 40, 50 , 60};
+    int arr2[5] = {10, 20, 30, 40, 50 };
 
     // int n1, n2;
     // cout << "Enter the size of the array1: ";
@@ -49,6 +53,8 @@ int main() {
 
 
 
-    cout << float(medianOf2UnequalSortedArrayUNOPTIMIZED(n1, n2, arr1, arr2));
+    cout << fixed << setprecision(2) << medianOf2UnequalSortedArrayUNOPTIMIZED(n1, n2, arr1, arr2);
     return 0;
 }
+
+//TIME
