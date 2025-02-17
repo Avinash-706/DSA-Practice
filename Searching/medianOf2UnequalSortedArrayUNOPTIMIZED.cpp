706e93371d9335d075e2f98f9be5030e986ecaaf
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-02-17 19:59:42 +0530

    Latest Commit

--- Searching/medianOf2UnequalSortedArrayUNOPTIMIZED.cpp
+++ Searching/medianOf2UnequalSortedArrayUNOPTIMIZED.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 
-void medianOf2UnequalSortedArrayUNOPTIMIZED(int n1, int n2, int arr1[], int arr2[]){
+double medianOf2UnequalSortedArrayUNOPTIMIZED(int n1, int n2, int arr1[], int arr2[]){
     int n = n1+n2;
     int final[n];
     for(int i=0 ; i < n ; i++){
@@ -16,25 +16,25 @@ void medianOf2UnequalSortedArrayUNOPTIMIZED(int n1, int n2, int arr1[], int arr2
         else if(i >= n1) final[i] = arr2[i - n1]; 
     }
     sort(final, final + n);
-    if(n % 2 == 0)    cout << final[n / 2] << " , " << final[(n / 2) + 1];
-    else    cout << final[n/2];  
+    if(n % 2 == 0)    return    (final[n / 2] + final[(n / 2) + 1])/2;
+    else    return  final[n/2];  
 
 
 }
 
-void input(int n, int arr[]){
-    cout << "Enter the elements in the increasing order ( Note: sorted order): ";
-    for(int i=0; i < n; i++){
-        int input;
-        cin >> input;
-        arr[i] = input;
-    }
-}
+// void input(int n, int arr[]){
+//     cout << "Enter the elements in the increasing order ( Note: sorted order): ";
+//     for(int i=0; i < n; i++){
+//         int input;
+//         cin >> input;
+//         arr[i] = input;
+//     }
+// }
 
 int main() {
-    int n1 = 6, n2 = 5;
+    int n1 = 6, n2 = 6;
     int arr1[6] = {1, 2, 3, 4, 5, 6};
-    int arr2[5] = {10, 20, 30, 40, 50};
+    int arr2[6] = {9, 20, 30, 40, 50 , 60};
 
     // int n1, n2;
     // cout << "Enter the size of the array1: ";
@@ -49,6 +49,6 @@ int main() {
 
 
 
-    medianOf2UnequalSortedArrayUNOPTIMIZED(n1, n2, arr1, arr2);
+    cout << float(medianOf2UnequalSortedArrayUNOPTIMIZED(n1, n2, arr1, arr2));
     return 0;
 }
