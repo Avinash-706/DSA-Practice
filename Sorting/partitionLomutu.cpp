8235d37fc10f815671e8663b7976fae1f5045f58
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-03-02 21:51:42 +0530

    Latest Commit

---
+++ Sorting/partitionLomutu.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <string.h>
+#include <limits>
+using namespace std;
+
+// IN   - 10, 80, 30, 90, 40, 50, 70
+// IN   - 5
+// OUT  - 10  30  40  50  80  70  90
+
+
+void lomutuPartition(int n, int arr[], int p, int l, int h){
+    swap(arr[h], arr[p]);
+    int pivot = arr[h];
+    int i = l-1;
+    for(int j = l ; j <= h - 1; j++){
+        if(arr[j] < pivot){
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i+1], arr[h]);
+}
+
+
+int main() {
+    // int n = 7;
+    // int arr[7] = {10, 80, 30, 90, 40, 50, 70};
+    // int p = 5;
+
+    int n, p;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements: ";
+    for(int i=0; i < n; i++){
+        int input;
+        cin >> input;
+        arr[i] = input;
+    }
+    cout << "Enter the Index of Element from which we want partition: ";
+    cin >> p;
+
+    lomutuPartition(n, arr, p , 0, n-1);
+    for(int i : arr)    cout << i << " ";
+    return 0;
+
+}
+
+// TIME  COMPLEXITY  - O(n), where 'n' is the size of the array
+// SPACE COMPLEXITY  - O(1)
