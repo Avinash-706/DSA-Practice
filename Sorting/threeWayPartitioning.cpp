f8e1811ef32c8d0985c60cf18fbed438ec3cb711
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-03-04 23:08:56 +0530

    Latest Commit

---
+++ Sorting/threeWayPartitioning.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <string.h>
+#include <limits>
+using namespace std;
+
+// DUTCH NATIONAL FLAG ALGORITHM
+void threeWayPartition(int n , int arr[]){
+
+    int l = 0, h = n - 1, mid = 0;
+    while(mid <= h){
+        if(arr[mid] == 0){
+            swap(arr[mid], arr[l]);
+            l++;
+            mid++;
+        }
+        else if(arr[mid] == 1)    mid++;
+        else{
+            swap(arr[mid], arr[h]);
+            h--;
+        }
+    }
+}
+
+
+int main() {
+    // int n = 6;
+    // int arr[6] = {0, 1, 0, 2, 1, 2}; 
+
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements(Note: Only Enter 0, 1 and 2): ";
+    for(int i=0; i < n; i++){
+        int input;
+        cin >> input;
+        arr[i] = input;
+    }
+
+    threeWayPartition(n, arr);
+    for(int i : arr)    cout << i << " ";
+    return 0;
+}
