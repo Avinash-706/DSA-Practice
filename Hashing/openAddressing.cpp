2ebe7850e9427185c10fe0380fd36cbf683eb15c
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-03-14 12:59:49 +0530

    Latest Commit

---
+++ Hashing/openAddressing.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <string.h>
+#include <limits>
+using namespace std;
+
+struct MyHash{
+    int *arr;
+    int cap, size;
+
+    MyHash(int c){
+        cap = c;
+        size = 0;
+        for(int i = 0 ; i < cap ; i++)    arr[i] = -1;
+    }
+
+    int hash(int key){
+        return key % cap;
+    }
+
+    bool insert(int key){
+        if(size == cap)    return false;
+        int i = hash(key);
+        while(arr[i] != -1 && arr[i] != -2 && arr[i] != key)    i = (i+1) % cap;
+        if(arr[i] == key)   return false;
+        else{
+            arr[i] = key;
+            size++;
+            return true;
+        }
+    }
+
+    bool erase(int key){
+        int h = hash(key);
+        int i = h;
+        while(arr[i] != -1){
+            if(arr[i] == key){
+                arr[i] = -2;
+                return true;
+            }
+
+            i = (i + 1) % cap;
+            if(i == h)  return false;
+            return false;
+        }
+    }
+
+    bool search(int key){
+        int h = hash(key);
+        int i = h;
+
+        while(arr[i] != -1){
+            if(arr[i] == key)   return true;
+            i = (i+1)%cap;
+            if(i == h)    return false;
+        }
+        return false;
+    }
+};
+
+
+
+int main() {
+    MyHash mh(7);
+    mh.insert(49);
+    mh.insert(56);
+    mh.insert(72);
+
+    cout << endl << " -- Searching for 56 -- " << endl;
+    if(mh.search(56) == true)   cout << endl << "Present" << endl;
+    else    cout << endl << "Not Present" << endl;
+
+    cout << endl << " -- Removing 56 -- " << endl;
+    mh.erase(56);
+
+    cout << endl << " -- Searching for 56 (AFTER DELETION) -- " << endl;
+    if(mh.search(56) == true)   cout << endl << "Present" << endl;
+    else    cout << endl << "Not Present" << endl;
+    
+    return 0;
+}
