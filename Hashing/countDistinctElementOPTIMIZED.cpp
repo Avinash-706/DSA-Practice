d73ed787682b03615f8795c46badd24920c17a16
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-03-15 15:34:16 +0530

    Latest Commit

---
+++ Hashing/countDistinctElementOPTIMIZED.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <string.h>
+#include <limits>
+using namespace std;
+
+// OPTIMIZED APPROACH
+int countDistinctElementOPTIMIZED(int n, int arr[]){
+    unordered_set <int> s;
+    for(int i = 0 ; i < n ; i++){
+        s.insert(arr[i]);
+    }
+    return s.size();
+}
+
+
+int main() {
+    // int n = 5;
+    // int arr[5] = {2, 10, 2, 8, 8};
+
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements: ";
+    for(int i=0; i < n; i++){
+        int input;
+        cin >> input;
+        arr[i] = input;
+    }
+
+    cout << "The Count of Distinct Elements : " << countDistinctElementOPTIMIZED(n, arr);
+    return 0;
+}
+
+// TIME  COMPLEXITY  : O(n), where 'n' is the size of the array  
+// SPACE COMPLEXITY  : O(n), since an unordered_set is used to store distinct elements  
