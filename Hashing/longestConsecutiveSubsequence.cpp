2a761fc74c3b69f633dc8c1b2cecbebfced28b98
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-03-21 17:39:08 +0530

    Latest Commit

--- Hashing/longestConsecutiveSubsequence.cpp
+++ Hashing/longestConsecutiveSubsequence.cpp
@@ -8,37 +8,36 @@
 using namespace std;
 
 
-int compute(int n, int arr[]){
-    sort(arr, arr+n);
-    int maxLen = 0;
-    int len = 0;
-    for(int i = 1; i < n; i++){
-        if(arr[i] == arr[i-1] + 1 )    len++;
-        else{
-            len  = 0;
-            maxLen = max(maxLen , len);
-        }
+int longestConsecutiveSubsequence(int n, int arr[]){
+    if (n == 0)    return 0;  
+    sort(arr, arr + n);
+    int maxLen = 1, len = 1;
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] == arr[i - 1])   continue;     //for duplicate
+        if (arr[i] == arr[i - 1] + 1)   len++;
+        else    len = 1;  
+        maxLen = max(maxLen, len);
     }
     return maxLen;
-    
 }
 
 
 int main() {
-    int n = 6;
-    int arr[6] = {1, 9, 3, 4, 2, 20};
+    // int n = 6;
+    // int arr[6] = {1, 9, 3, 4, 2, 20};
 
-    // int n;
-    // cout << "Enter the size of the array: ";
-    // cin >> n;
-    // int arr[n];
-    // cout << "Enter the elements: ";
-    // for(int i=0; i < n; i++){
-    //     int input;
-    //     cin >> input;
-    //     arr[i] = input;
-    // }
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements: ";
+    for(int i=0; i < n; i++){
+        int input;
+        cin >> input;
+        arr[i] = input;
+    }
 
-    cout << compute(n, arr);
+    cout << "The Longest Consecutive Subsequence  : " << longestConsecutiveSubsequence(n, arr);
     return 0;
 }
