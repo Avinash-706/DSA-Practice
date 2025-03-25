edb5d7ddd4ec893293c4736451f2427b3cf36073
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-03-25 18:40:07 +0530

    Latest Commit

--- Hashing/countDistinctElementInEveryWindow.cpp
+++ Hashing/countDistinctElementInEveryWindow.cpp
@@ -9,37 +9,36 @@ using namespace std;
 
 
 void countDistinctElementInEveryWindow(int n, int arr[], int windowSize){
-    int windowCount = n - windowSize + 1;
-    for(int i = 0; i < n ; i++){
-        int k = windowCount;
+    for(int i = 0; i < (n - windowSize + 1) ; i++){
         unordered_set<int> s;
-        while(k > 0){
-            s.insert()
-            k--;
-            i++;
+        for(int j = i ; j < i + windowSize; j++){
+            s.insert(arr[j]);
         }
+        cout << s.size() << "  ";
     }
 
 }
 
-
 int main() {
-    int n = 7;
-    int arr[7] = {10, 20, 20, 10, 30, 40, 10};
-    int windowSize = 4;
+    // int n = 7;
+    // int arr[7] = {10, 20, 20, 10, 30, 40, 10};
+    // int windowSize = 4;
 
 
-    // int n;
-    // cout << "Enter the size of the array: ";
-    // cin >> n;
-    // int arr[n];
-    // cout << "Enter the elements: ";
-    // for(int i=0; i < n; i++){
-    //     int input;
-    //     cin >> input;
-    //     arr[i] = input;
-    // }
+    int n, windowSize;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];  
+    cout << "\nEnter the elements of the array: ";
+    for(int i = 0; i < n; i++)    cin >> arr[i];
+
+    cout << "\nEnter the window size: ";
+    cin >> windowSize;
 
     countDistinctElementInEveryWindow(n, arr, windowSize);
     return 0;
 }
+
+// TIME COMPLEXITY - O(n * k), where 'n' is the size of the array and 'k' is the window size.
+//SPACE COMPLEXITY - O(k), where 'k' is the window size
