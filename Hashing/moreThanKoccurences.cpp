aedef3a413edf7940ba9d8b5f38c22ce6b65e053
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-03-26 19:23:38 +0530

    Latest Commit

---
+++ Hashing/moreThanKoccurences.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <string.h>
+#include <limits>
+using namespace std;
+
+
+//MORE THAN n/k OCURRENCES SHOULD BE THERE
+void moreThanKoccurences(int n, int arr, int k){
+    unordered_map<int, int> h;
+    for(int i = 0 ; i < n ; i++)
+   
+}
+
+
+int main() {
+    int n = 8;
+    int arr[8] = {30, 10, 20, 20, 10, 20, 30, 30};
+    int k = 4;
+
+    // int n;
+    // cout << "Enter the size of the array: ";
+    // cin >> n;
+    // int arr[n];
+    // cout << "Enter the elements: ";
+    // for(int i=0; i < n; i++){
+    //     int input;
+    //     cin >> input;
+    //     arr[i] = input;
+    // }
+
+    moreThanKoccurences(n, arr, k);
+    return 0;
+}
