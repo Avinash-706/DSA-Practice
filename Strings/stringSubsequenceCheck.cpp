f96fdf3bd53ea0be5de629c4653d232a7d938650
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-03-31 01:13:31 +0530

    Latest Commit

--- Strings/stringSubsequenceCheck.cpp
+++ Strings/stringSubsequenceCheck.cpp
@@ -1,31 +1,23 @@
 #include <iostream>
-#include <bits/stdc++.h>
-#include <iomanip>
-#include <algorithm>
 #include <string>
-#include <string.h>
-#include <limits>
 using namespace std;
 
-
-bool compute(string s1, string s2){
+bool isSubsequence(const string& s1, const string& s2) {
     int j = 0;
-    for(int i = 0 ; i < s1.length() && j < s2.length() ; i++){
-        if(s1[i] == s2[j])    j++;
-    }
-    return    j == s2.length();
+    for (int i = 0; i < s1.length() && j < s2.length(); i++)    if (s1[i] == s2[j])    j++;
+    return j == s2.length();
 }
 
-
 int main() {
-    string str1 = "abcde";
-    string str2 = "ae";
+    // string str1 = "abcde";
+    // string str2 = "acbe";
 
-    // cout << "Enter a string1: ";
-    // cin >> str1;
-    // cout << "Enter a string2: ";
-    // cin >> str2;
+    string str1, str2;
+    cout << "Enter a string1: ";
+    cin >> str1;
+    cout << "Enter a string2: ";
+    cin >> str2;
 
-    cout << " str2  is a SubSequence of str1  : " << (compute(str1, str2) ? "Yes" : "No");
+    cout << "str2 is a SubSequence of str1: " << (isSubsequence(str1, str2) ? "Yes" : "No") << endl;
     return 0;
 }
