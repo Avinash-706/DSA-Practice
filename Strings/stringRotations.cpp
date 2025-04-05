2c6cee0fe9b62d3626d3a12107f298247ab8f43e
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-04-05 17:50:02 +0530

    Latest Commit

---
+++ Strings/stringRotations.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <string.h>
+#include <limits>
+using namespace std;
+
+//UNOPTIMIZED APPROACH
+
+// void callRotate(string &s){
+//     int n = s.length();
+//     char temp = s[n - 1];
+//     for(int i = n - 1; i > 0; i--){
+//         s[i] = s[i - 1];
+//     }
+//     s[0] = temp;
+// }
+
+void stringRotations(string &s1, string &s2){
+
+    if(s1.length() != s2.length()){
+        cout << "-1" << endl;
+        return;
+    }
+    
+    for(int i = 0 ; i < s1.length(); i++){
+        //approach1:
+        rotate(s2.begin(), s2.begin() + 1, s2.end());
+
+        // // approach 2:
+        // callRotate(s2);
+
+        if(s1 == s2){
+            cout << "TRUE" << endl; 
+            return ;
+        }  
+    }
+
+}
+
+
+int main() {
+    string s1 = "abcde";
+    string s2 = "bcdea";
+
+    string s1, s2;
+    cout << "Enter the original string: ";
+    cin >> s1;
+    cout << "Enter the rotated string to check: ";
+    cin >> s2;
+
+    stringRotations(s1, s2);
+    return 0;
+}
+
+// TIME  COMPLEXITY  : O(n^2), where 'n' is the length of the string
+// SPACE COMPLEXITY  : O(1)  
