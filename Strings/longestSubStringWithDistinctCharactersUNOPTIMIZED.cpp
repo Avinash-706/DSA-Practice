a85c693f4f168d6762423ddc6b78815178113e1e
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-04-06 13:17:54 +0530

    Latest Commit

--- Strings/longestSubStringWithDistinctCharactersUNOPTIMIZED.cpp
+++ Strings/longestSubStringWithDistinctCharactersUNOPTIMIZED.cpp
@@ -7,17 +7,17 @@
 #include <limits>
 using namespace std;
 
+// IN  - abcdabc
+// OUT - 4 (cdab)
 
+//UNOPTIMIZED APPROACH
 size_t longestSubStringWithDistinctCharacters(string str){
     int n = str.length();
-    size_t len = 1;
+    size_t len = 0;
     for(int i = 0 ; i < n ; i++){
         unordered_set<char> s;
         for(int j = i ; j < n ; j++){
-            if(s.find(str[i]) != s.end()){
-                len = max(s.size(), len);
-                break;
-            }
+            if(s.find(str[j]) != s.end())   break;
             else{
                 s.insert(str[j]);
                 len =  max(s.size(), len);
@@ -27,14 +27,16 @@ size_t longestSubStringWithDistinctCharacters(string str){
     return len;
 }
 
-
 int main() {
-    string str = "abcdabc";
+    // string str = "abcdabc";
 
-    // string str;
-    // cout << "Enter a string: ";
-    // cin >> str;
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
 
-    cout << longestSubStringWithDistinctCharacters(str);
+    cout << "The Length of the Longest SubString with Distinct Characters : " << longestSubStringWithDistinctCharacters(str);
     return 0;
 }
+
+// TIME  COMPLEXITY : O(n²), where 'n' is the length of the string
+// SPACE COMPLEXITY : O(n), where 'n' is the max size of unordered_set
