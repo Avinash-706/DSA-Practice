612cc8e44309aed252611a36cf070af1ea3debd9
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-04-09 00:38:51 +0530

    Latest Commit

---
+++ LinkedList/Operations/traversePrint.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <string.h>
+#include <limits>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *next;
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+void printLinkedList(Node *head){
+    Node *curr = head;
+    while(curr != NULL){
+        cout << curr -> data << "  ";
+        curr = curr -> next;
+    }
+
+}
+
+int main() {
+    Node *head = new Node(10);
+    head -> next = new Node(20); 
+    head -> next -> next = new Node(30); 
+    
+    printLinkedList(head);
+
+    return 0;
+}
