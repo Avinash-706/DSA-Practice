f093e3dab6ac791dd8270dc219f73452065bcb47
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-04-14 08:04:45 +0530

    Latest Commit

---
+++ LinkedList/Operations/Deletion/deleteFirstNode.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <string.h>
+#include <limits>
+using namespace std;
+
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node (int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+void printList(Node *head){
+    Node *curr = head;
+    while(curr != NULL){
+        cout << curr -> data << "  ";
+        curr = curr -> next;
+    }
+    cout << endl;
+}
+
+void deleteFirstNode(Node *head){
+    if(head == NULL){
+        cout << " NULL ";
+        return ;
+    }
+    
+    
+    
+}
+
+int main() {
+    Node *head;
+    head = new Node(10);
+    head -> next =  new Node(20);
+    head -> next -> next =  new Node(30);
+    head -> next -> next -> next =  new Node(40);
+    printList(head);
+
+    deleteFirstNode(head);
+    return 0;
+}
