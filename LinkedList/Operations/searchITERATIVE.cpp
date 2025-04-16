474b8afaa1f24338320625c7e46739fcea5b6626
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-04-16 10:54:07 +0530

    Latest Commit

---
+++ LinkedList/Operations/searchITERATIVE.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <string.h>
+#include <limits>
+using namespace std;
+
+
+struct Node {
+    int data;
+    Node *next;
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+void printLinkedList(Node *head){
+    Node *curr = head;
+    while(curr != NULL){
+        cout << curr -> data << "  ";
+        curr = curr -> next;
+    }
+    cout << endl;
+}
+
+
+int search(Node *head, int num){
+    Node *curr = head;
+    int i = 1;
+    while(curr != NULL){
+        if(curr -> data == num)     return i;
+        i++;
+        curr = curr -> next;
+    }
+}
+
+
+int main() {
+    Node *head = NULL;
+    head = new Node(10);
+    head -> next = new Node(20); 
+    head -> next -> next = new Node(30); 
+    head -> next -> next -> next = new Node(40);
+    
+    printLinkedList(head);
+
+    cout << "The position of 30 : " << search(head, 30);
+    return 0;
+}
