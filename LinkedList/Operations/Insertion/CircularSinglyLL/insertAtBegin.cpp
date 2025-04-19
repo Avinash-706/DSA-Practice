1ba490f9322adb51c508d1b6c3531f82705f7f0b
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-04-19 22:27:49 +0530

    Latest Commit

---
+++ LinkedList/Operations/Insertion/CircularSinglyLL/insertAtBegin.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <string.h>
+#include <limits>
+using namespace std;
+
+
+struct Node{
+    int data;
+    Node *next;
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+void printLinkedList(Node *head){
+    if(head == NULL){
+        cout << "NULL" << endl;
+        return;
+    }
+
+    cout << head -> data << "  ";
+    for(Node *p = head -> next ; p != head ; p = p -> next){
+        cout << p -> data << "  ";
+    } 
+}
+
+
+Node *insertAtBegin(Node *head, int num){
+    if (head == NULL) {
+        head = new Node(num);
+        head->next = head;
+        return head;
+    }
+    
+
+    Node *temp = new Node(num);
+    temp -> next = head;
+
+    Node *curr = head;
+    while(curr -> next != head){
+        curr = curr -> next;
+    }
+    curr -> next = temp;
+    head = temp;
+    return temp;
+}
+
+
+int main() {
+    Node *head = new Node(10);
+    head -> next = new Node(20); 
+    head -> next -> next = new Node(30); 
+    head -> next -> next -> next = head; 
+
+    head = insertAtBegin(head, 5);
+    
+    printLinkedList(head);
+    return 0;
+}
+
+// TIME  COMPLEXITY  : O(n)
+// SPACE COMPLEXITY  : O(1)
