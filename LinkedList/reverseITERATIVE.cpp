1ab4fad116a20d98dfceeeecb3634f3640b7ab51
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-04-28 23:57:47 +0530

    Latest Commit

---
+++ LinkedList/reverseITERATIVE.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+//OPTIMIZED APPROACH : by changing the next pointers
+struct Node{
+    int data;
+    Node *next;
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+void printLinkedList(Node *head){
+    Node *curr = head;
+    while(curr != NULL){
+        cout << curr->data << "  ";
+        curr = curr->next;
+    }
+    cout << endl;
+}
+
+
+Node *reverse(Node *head){
+    Node *prev = NULL;
+    Node *curr = head;
+    while(curr != NULL){
+        Node *next = curr -> next;
+        curr -> next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+
+}
+
+
+int main() {
+    Node *head = new Node(10);
+    head->next = new Node(20); 
+    head->next->next = new Node(30); 
+    head->next->next->next = new Node(40);
+    head->next->next->next->next = new Node(50);
+
+    cout << "Original Linked List :  ";
+    printLinkedList(head);
+
+    head = reverse(head);
+    cout << "Reversed Linked List :  ";
+    printLinkedList(head);
+
+    return 0;
+}
+
+// TIME  COMPLEXITY  : O(n), where 'n' is the number of nodes in the LinkedList
+// SPACE COMPLEXITY  : O(1)
