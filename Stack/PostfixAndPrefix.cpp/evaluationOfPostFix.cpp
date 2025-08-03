300329319f63f150d3db068757c4b3a38d25b9a0
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-08-03 19:28:16 +0530

    Evaluation PostFix Conversion --Stack

---
+++ Stack/PostfixAndPrefix.cpp/evaluationOfPostFix.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <stack>
+#include <cmath>
+#include <string>
+#include <cctype>
+using namespace std;
+
+// POSTFIX EVALUATION
+struct Stack {
+    int cap, top;
+    string output;
+    char* arr;
+
+    Stack(int x) {
+        top = -1;
+        cap = x;
+        arr = new char[cap];
+        output = "";
+    }
+
+    bool isEmpty() {
+        return top == -1;
+    }
+
+    void push(char x) {
+        arr[++top] = x;
+    }
+
+    char pop() {
+        if (!isEmpty()) {
+            top--;
+            return arr[top + 1];
+        }
+        else {
+            return '$';
+        }
+    }
+
+    char peek() {
+        if (!isEmpty()) return arr[top];
+        else return '$';
+    }
+
+    long long calculateValue(string exp) {
+        stack<long long> s;
+
+        for (int i = 0; i < exp.length(); i++) {
+            if (isdigit(exp[i])) {
+                s.push(exp[i] - '0');
+            }
+            else {
+                if (s.size() < 2) {
+                    cout << "Invalid postfix expression" << endl;
+                    return -1;
+                }
+
+                long long i1 = s.top(); s.pop();
+                long long i2 = s.top(); s.pop();
+                long long result = 0;
+
+                switch (exp[i]) {
+                    case '+': 
+                        result = i2 + i1;
+                        break;
+                    case '-': 
+                        result = i2 - i1; 
+                        break;
+                    case '*': 
+                        result = i2 * i1;
+                        break;
+                    case '/':
+                        if (i1 == 0) {
+                            cout << "Division by zero" << endl;
+                            return -1;
+                        }
+                        result = i2 / i1;
+                        break;
+                    case '^':
+                        result = pow(i2, i1);
+                        break;
+                    default:
+                        cout << "Invalid operator: " << exp[i] << endl;
+                        return -1;
+                }
+                s.push(result);
+            }
+        }
+
+        if (s.size() != 1) {
+            cout << "Invalid postfix expression" << endl;
+            return -1;
+        }
+
+        return s.top();
+    }
+};
+
+int main() {
+    string exp1 = "242^^"; 
+    string exp2 = "231*+9-";
+
+    Stack obj(100);
+
+    cout << "\nPOSTFIX : " << exp1 << "\nVALUE   : " << obj.calculateValue(exp1) << endl << endl;
+    cout << "\nPOSTFIX : " << exp2 << "\nVALUE   : " << obj.calculateValue(exp2) << endl << endl;
+
+    return 0;
+}
+
+// TIME  COMPLEXITY  : O(n + m × log b), where
+//                      n = length of postfix expression
+//                      m = number of '^' operators
+//                      b = maximum exponent value
+
+// SPACE COMPLEXITY : O(n), for operand stack during evaluation
+
