3dfc02bcb33e35067049c87ef331cb53d99895fc
Avinash Dhanuka <avunashdhanuka@gmail.com>
2025-12-16 21:49:00 +0530

    Ceiling On The Left In Array

---
+++ BinarySearchTree/ceilingOnLeftInArray.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+
+void printCeiling(int arr[], int n)
+{
+    set<int> s;
+
+    cout << "-1" << " ";
+    s.insert(arr[0]);
+
+    for(int i = 1; i < n; i++)
+    {
+        auto it = s.lower_bound(arr[i]);
+
+        if(it != s.end())
+            cout << *it << " ";
+        else
+            cout << "-1" << " ";
+
+        s.insert(arr[i]);
+    }
+}
+
+int main()
+{
+    int arr[] = {2, 8, 30, 15, 25, 12};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Ceiling on the Left In Array : ";
+    printCeiling(arr, n);
+
+    return 0;
+}
